fcc6c1275be9f5d8cc5ab71231e29b0023514df8
sj3kenne <32079919+sj3kenne@users.noreply.github.com>
2020-05-30 02:00:24 -0400

    created react app

---
+++ portfolio/src/Projects.js
@@ -0,0 +1,81 @@
+import React from "react"
+import Thumbnail from './Thumbnail.js'; // Import the Thumbnail component
+
+// Import the BrowserRouter, Route and Link components
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Embody from './embody-headrest.js';
+
+
+import './App.css';
+
+function Projects(props) {
+    return (
+        <BrowserRouter>
+            <div className="Projects">
+
+                <Route path="/embody-headrest" component={Embody} />
+
+
+                <h1>Projects</h1>
+                <Thumbnail
+                    link="/embody-headrest"
+                    image="http://twitter-image-url.jpg"
+                    title="Embody Headrest"
+                    category="3D Design"
+                />
+
+                <Thumbnail
+                    link="/donut"
+                    image="http://airbnb-image-url.jpg>"
+                    title="Blender Donut"
+                    category="3D Design"
+                />
+
+                <Thumbnail
+                    link="/cherry-keys"
+                    image="http://photoshop-image-url.jpg"
+                    title="CherryMX Typewriter Keycap Design"
+                    category="3D Design"
+                />
+
+                <Thumbnail
+                    link="/hyrdoponics"
+                    image="http://photoshop-image-url.jpg"
+                    title="40 Plant Hydroponic Wall"
+                    category="DIY"
+                />
+
+                <Thumbnail
+                    link="/longboard"
+                    image="http://photoshop-image-url.jpg"
+                    title="Longboards"
+                    category="DIY"
+                />
+
+                <Thumbnail
+                    link="/heliodox-tent"
+                    image="http://photoshop-image-url.jpg"
+                    title="Helidox (CRKBD) keyboard tenting and case redesign"
+                    category="3D Design"
+                />
+
+                <Thumbnail
+                    link="/cakes"
+                    image="http://photoshop-image-url.jpg"
+                    title="Gallery of Cakes"
+                    category="Baking"
+                />
+
+                <Thumbnail
+                    link="/Insoles"
+                    image="http://photoshop-image-url.jpg"
+                    title="Insole Scanning and Printing"
+                    category="3D Design"
+                />
+
+            </div>
+        </BrowserRouter>
+    )
+}
+
+export default Projects;
---
+++ portfolio/src/embody-headrest.js
@@ -0,0 +1,11 @@
+import React from "react"
+ 
+function Embody(props) {
+  return (
+    <div>
+      <h1>Embody Headrest</h1>
+    </div>
+  )
+}
+ 
+export default Embody;
