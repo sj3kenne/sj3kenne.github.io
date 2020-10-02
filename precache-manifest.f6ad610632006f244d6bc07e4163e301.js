dba3e929132f6290f128a2d8c4801429907554a6
sj3kenne <32079919+sj3kenne@users.noreply.github.com>
2020-10-02 12:34:54 -0400

    Updates

---
+++ precache-manifest.f6ad610632006f244d6bc07e4163e301.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edea906fccf00c1c5b701a5756bd50d2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9ea54a1aa3e55200807b",
+    "url": "./static/css/2.62afb475.chunk.css"
+  },
+  {
+    "revision": "c4ca7c81aaf1d577b48f",
+    "url": "./static/css/main.9e68e815.chunk.css"
+  },
+  {
+    "revision": "9ea54a1aa3e55200807b",
+    "url": "./static/js/2.b34eeb64.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.b34eeb64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4ca7c81aaf1d577b48f",
+    "url": "./static/js/main.3a90b956.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  },
+  {
+    "revision": "92e492d06b8f76448c702e7e1d6be3d2",
+    "url": "./static/media/0030.92e492d0.png"
+  },
+  {
+    "revision": "d65228afc39b21e8f6e9b8d8ef55056d",
+    "url": "./static/media/SamKennedy.d65228af.JPG"
+  },
+  {
+    "revision": "77fbd4db73a7503f264ac1a0f887c937",
+    "url": "./static/media/almost-final-render.77fbd4db.png"
+  },
+  {
+    "revision": "482ac3c23b3c7db8b2d547a1ce34163e",
+    "url": "./static/media/cake-wedding1.482ac3c2.jpg"
+  },
+  {
+    "revision": "06f24ceec4fe79730d37b2439b16acd4",
+    "url": "./static/media/cherry-stem.06f24cee.JPG"
+  },
+  {
+    "revision": "c1d617ffb541eb5696d3131e70b744d8",
+    "url": "./static/media/donut-first-render.c1d617ff.png"
+  },
+  {
+    "revision": "c7b2ddbf4a46d4efabc1f994626792aa",
+    "url": "./static/media/donut-fourth-render.c7b2ddbf.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "69351e1d566f0580b02c6bb8f0701f04",
+    "url": "./static/media/headrest-final-physical1.69351e1d.jpg"
+  },
+  {
+    "revision": "d6d00cf549349258d78dad707897b356",
+    "url": "./static/media/hydro-first-iteration.d6d00cf5.jpg"
+  },
+  {
+    "revision": "05afc74fa716cd17e162dcdf5761779a",
+    "url": "./static/media/hydro-poles.05afc74f.jpg"
+  },
+  {
+    "revision": "9a74b7bec04853a2bef41ffbc0afe365",
+    "url": "./static/media/hydro-poles2.9a74b7be.jpg"
+  },
+  {
+    "revision": "5305f322bf3029e7a35dbea66c6df7d6",
+    "url": "./static/media/hydro-second-iteration.5305f322.jpg"
+  },
+  {
+    "revision": "2a91bc9984c766d316034ab19a9b4b1b",
+    "url": "./static/media/hydro-top.2a91bc99.jpg"
+  },
+  {
+    "revision": "57388024a5beffa93c4983bb888c9d8f",
+    "url": "./static/media/hyrdo-screw-holes.57388024.jpg"
+  },
+  {
+    "revision": "2eee7a0a03f5d06dd1f9a360e6b4b225",
+    "url": "./static/media/keyless-ergodox.2eee7a0a.jpeg"
+  },
+  {
+    "revision": "2f307a5951dcf20787c200f77a8c4b86",
+    "url": "./static/media/optimize.2f307a59.gif"
+  },
+  {
+    "revision": "c6eaaf77d5665ac8da5940ca7c95e384",
+    "url": "./static/media/pre-final-render.c6eaaf77.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sj3kenne.github.io/precache-manifest.6b8b874d3955c7dc4627726f7668c158.js"
+  "./precache-manifest.f6ad610632006f244d6bc07e4163e301.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sj3kenne.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
